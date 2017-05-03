f1eac0d51f657d027722af2e8cd9c79eeced5b9c
Willy Aguirre <marti1125@gmail.com>
2017-05-02 19:34:08 -0500

    fix create token

--- src/culqi/token.rs
+++ src/culqi/token.rs
@@ -1,12 +1,34 @@
 extern crate serde_json;
 
 use client::Client;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Token {
 }
 
 impl Token {
+
+    pub fn new<S: Into<String>>(
+        card_number: S,
+        cvv: S,
+        expiration_month: i32,
+        expiration_year: i32,
+        email: S
+    ) -> HashMap<String, serde_json::Value> {
+        let mut map: HashMap<String, serde_json::Value>;
+        map = HashMap::new();
+        map.insert("card_number".to_string(), json!(card_number.into()));
+        map.insert("cvv".to_string(), json!(cvv.into()));
+        map.insert("expiration_month".to_string(), json!(expiration_month));
+        map.insert("expiration_year".to_string(), json!(expiration_year));
+        map.insert("email".to_string(), json!(email.into()));
+        return map;
+    }
+
+    pub fn create(client: &Client, token: &HashMap<String, serde_json::Value>) -> String {
+        client.post("/tokens", token)
+    }
 
     pub fn retrieve(client: &Client, id: &str) -> String {
         client.get(&format!("/tokens/{}", id))
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@ extern crate serde;
 extern crate serde_json;
 extern crate culqi;
 
+use std::env;
 use serde_json::{Value};
 
 fn main() {
 
-    //let new_token = culqi::Token::new("4111111111111111","123","03","20","test@test.com");
+    let public_key = env::var("PUBLIC_KEY").unwrap();
+    let secret_key = env::var("SECRET_KEY").unwrap();
 
-    //let client_pk = culqi::Client::new("pk_test_vzMuTHoueOMlgUPj");
-    //let client_sk = culqi::Client::new("sk_test_UTCQSGcXW8bCyU59");
+    let client_pk = culqi::Client::new(&public_key);
+    let client_sk = culqi::Client::new(&secret_key);
+
+    let new_token = culqi::Token::new("4111111111111111","123",9, 2020,"test@test.com");
 
     //let get_token = culqi::Token::create(&client_pk, &new_token);
 
     //let token_json: Value = serde_json::from_str(&get_token).unwrap();
 
     let new_charge = culqi::Charge::new("1000", "PE", "will@me.com", "ffff");
-
-    let client_sk = culqi::Client::new("sk_test_UTCQSGcXW8bCyU59");
 
     let get_charge = culqi::Charge::create(&client_sk, &new_charge);
 
