bce17285df7be838fd5934e2dbdfcf5279b905e2
Willy Aguirre <marti1125@gmail.com>
2017-05-16 01:30:35 -0500

    add method all for Subscription

--- src/culqi/subscription.rs
+++ src/culqi/subscription.rs
@@ -36,4 +36,58 @@ impl Subscription {
         client.get(&format!("/subscriptions/{}", id))
     }
 
+    pub fn all(
+        client: &Client,
+        amount: Option<String>,
+        min_amount: Option<String>,
+        max_amount: Option<String>,
+        creation_date: Option<String>,
+        creation_date_from: Option<String>,
+        creation_date_to: Option<String>,
+        interval: Option<String>,
+        status: Option<String>,
+        limit: Option<String>
+    ) -> String {
+        let mut query_url = String::from("/tokens");
+        query_url.push_str("?limit");
+        if limit.is_none() {
+            query_url.push_str("=50");
+        } else {
+            query_url.push_str(&format!("={:?}", limit));
+        }
+        if !amount.is_none() {
+            query_url.push_str("&amount");
+            query_url.push_str(&format!("={:?}", amount));
+        }
+        if !min_amount.is_none() {
+            query_url.push_str("&min_amount");
+            query_url.push_str(&format!("={:?}", min_amount));
+        }
+        if !max_amount.is_none() {
+            query_url.push_str("&max_amount");
+            query_url.push_str(&format!("={:?}", max_amount));
+        }
+        if !creation_date.is_none() {
+            query_url.push_str("&creation_date");
+            query_url.push_str(&format!("={:?}", creation_date));
+        }
+        if !creation_date_from.is_none() {
+            query_url.push_str("&creation_date_from");
+            query_url.push_str(&format!("={:?}", creation_date_from));
+        }
+        if !creation_date_to.is_none() {
+            query_url.push_str("&creation_date_to");
+            query_url.push_str(&format!("={:?}", creation_date_to));
+        }
+        if !interval.is_none() {
+            query_url.push_str("&interval");
+            query_url.push_str(&format!("={:?}", interval));
+        }
+        if !status.is_none() {
+            query_url.push_str("&status");
+            query_url.push_str(&format!("={:?}", status));
+        }
+        client.get(&query_url)
+    }
+
 }
