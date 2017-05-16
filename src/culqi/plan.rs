88d34bc01e199a52bc428c5f0ac00d4201b71f0e
Willy Aguirre <marti1125@gmail.com>
2017-05-16 01:45:03 -0500

    add method all for Plans

--- src/culqi/plan.rs
+++ src/culqi/plan.rs
@@ -48,4 +48,48 @@ impl Plan {
         client.get(&format!("/plans/{}", id))
     }
 
+    pub fn all(
+        client: &Client,
+        amount: Option<String>,
+        min_amount: Option<String>,
+        max_amount: Option<String>,
+        creation_date: Option<String>,
+        creation_date_from: Option<String>,
+        creation_date_to: Option<String>,
+        limit: Option<String>
+    ) -> String {
+        let mut query_url = String::from("/plans");
+        query_url.push_str("?limit");
+        if limit.is_none() {
+            query_url.push_str("=50");
+        } else {
+            query_url.push_str(&format!("={:?}", limit));
+        }
+        if !amount.is_none() {
+            query_url.push_str("&amount");
+            query_url.push_str(&format!("={:?}", amount));
+        }
+        if !min_amount.is_none() {
+            query_url.push_str("&min_amount");
+            query_url.push_str(&format!("={:?}", min_amount));
+        }
+        if !max_amount.is_none() {
+            query_url.push_str("&max_amount");
+            query_url.push_str(&format!("={:?}", max_amount));
+        }
+        if !creation_date.is_none() {
+            query_url.push_str("&creation_date");
+            query_url.push_str(&format!("={:?}", creation_date));
+        }
+        if !creation_date_from.is_none() {
+            query_url.push_str("&creation_date_from");
+            query_url.push_str(&format!("={:?}", creation_date_from));
+        }
+        if !creation_date_to.is_none() {
+            query_url.push_str("&creation_date_to");
+            query_url.push_str(&format!("={:?}", creation_date_to));
+        }
+        client.get(&query_url)
+    }
+
 }
