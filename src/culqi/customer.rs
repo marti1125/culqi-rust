fec4bda6153b9f6972c81b98f022ccf83f469874
Willy Aguirre <marti1125@gmail.com>
2017-05-21 13:00:00 -0500

    add document for Customer

--- src/culqi/customer.rs
+++ src/culqi/customer.rs
@@ -9,6 +9,52 @@ pub struct Customer {
 
 impl Customer {
 
+    //! # Examples
+    //!
+    //! ```
+    //! let client = culqi::Client::new("{KEY}");
+    //! ```
+    //!
+    //! ```
+    //! let new_customer = culqi::Customer::new("Will","Muro","wmuro@me.com","Av. Lima 123","LIMA","PE","999999999",None);
+    //! ```
+    //!
+    //! ```
+    //! let create_customer = culqi::Customer::create(&client, &new_customer);
+    //! ```
+    //!
+    //! ```
+    //! let get_customer = culqi::Customer::retrieve(&client, "{ID}");
+    //! ```
+    //!
+    //! ```
+    //! let delete_customer = culqi::Customer::delete(&client, "{ID}");
+    //! ```
+    //!
+    //!
+    //! Here you are the list of filters you can use for get Customers
+    //!
+    //! ```
+    //! use std::collections::HashMap;
+    //! ```
+    //!
+    //! ```
+    //! let mut customer_filter: HashMap<String, String>;
+    //! customer_filter = HashMap::new();
+    //! customer_filter.insert("first_name".to_string(), "Will".to_string());
+    //! customer_filter.insert("last_name".to_string(), "Muro".to_string());
+    //! customer_filter.insert("email".to_string(), "will@me.com".to_string());
+    //! customer_filter.insert("address".to_string(), "Av. Lima 123".to_string());
+    //! customer_filter.insert("address_city".to_string(), "LIMA".to_string());
+    //! customer_filter.insert("phone_number".to_string(), "99999999".to_string());
+    //! customer_filter.insert("country_code".to_string(), "PE".to_string());
+    //! ```
+    //!
+    //! ```
+    //! let all_customers = culqi::Customer::all(&client, Some(customer_filter), None, None, None);
+    //! ```
+    //!
+
     pub fn new<S: Into<String>>(
         first_name: S,
         last_name: S,
