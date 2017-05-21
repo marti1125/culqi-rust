ad098189b9d1a71f1db1fd2ea758025db45e9c8b
Willy Aguirre <marti1125@gmail.com>
2017-05-21 12:05:47 -0500

    add documentation for Refund

--- src/culqi/refund.rs
+++ src/culqi/refund.rs
@@ -9,6 +9,50 @@ pub struct Refund {
 
 impl Refund {
 
+    //! # Examples
+    //!
+    //! ```
+    //! let client = culqi::Client::new("{KEY}");
+    //! ```
+    //!
+    //! ```
+    //! let new_refund = culqi::Refund::new(100,"{CHARGE_ID}","fradudulento", None);
+    //! ```
+    //!
+    //! ```
+    //! let create_refund = culqi::Refund::create(&client, &new_refund);
+    //! ```
+    //!
+    //! ```
+    //! let get_refund = culqi::Refund::retrieve(&client, "{ID}");
+    //! ```
+    //!
+    //!
+    //! Here you are the list of filters you can use for get Refunds
+    //!
+    //! ```
+    //! use std::collections::HashMap;
+    //! ```
+    //!
+    //! ```
+    //! let mut reason_filter: HashMap<String, String>;
+    //! reason_filter = HashMap::new();
+    //! reason_filter.insert("reason".to_string(), "solicitud_comprador".to_string());
+    //! ```
+    //!
+    //! It must uses [Unix Timestamp](http://www.unixtimestamp.com/index.php)
+    //!
+    //! ```
+    //! reason_filter.insert("creation_date".to_string(), "1484006400".to_string());
+    //! reason_filter.insert("creation_date_from".to_string(), "1479600000".to_string());
+    //! reason_filter.insert("creation_date_to".to_string(), "1484006400".to_string());
+    //! ```
+    //!
+    //! ```
+    //! let all_refunds = culqi::Refund::all(&client, Some(reason_filter), None, None, None);
+    //! ```
+    //!
+
     pub fn new<S: Into<String>>(
         amount: i32,
         charge_id: S,
